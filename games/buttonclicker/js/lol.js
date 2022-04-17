180c91fb26318b81b6d3d41933e719d6fa8642c9
Henry Gal <103893247+sussybakadude@users.noreply.github.com>
2022-04-17 15:59:06 +0000

    Updated Button Clicker Code, Removed unused page

--- games/buttonclicker/js/lol.js
+++ games/buttonclicker/js/lol.js
@@ -1,1 +1,65 @@
-function _0xc465(){var _0x2deaf2=['yle=\x22color','ore\x20+=\x20par','n\x20id=\x22b\x22\x20o','1984600emTHoe','=\x20\x22Score:\x20','</span></p','r\x20add\x20one\x20','ton>','nction\x20che','charset=\x22u','\x20=\x20mult.to','\x20\x20<head>','\x20\x20\x20\x20<h1\x20st','score.js\x22>','lt\x20+=\x20pars','</button>','dscore()\x22>','\x22>0</span>','birth:\x20<sp','.innerHTML','\x20\x20backgrou','\x22addm\x22)\x20{','114518twarNC','=device-wi','\x20\x20\x20\x20\x20\x20\x20\x20do','range;','toString()','to\x20multi\x20o','198elvdcP','7PYaQzD','kie\x20=\x20\x22sco','2039015HYZNRO','er\x22))','rebirth\x20to','\x22addmulti(','Rebirth()\x22','\x20\x20\x20\x20<scrip','\x20\x20\x20\x20\x20\x20\x20\x20if','lick\x20the\x20S','utton\x20Clic','ect\x20of\x20min','lti:\x20<span','1000)</but','\x20\x20\x20\x20</styl','\x20\x20\x20\x20\x20\x20\x20\x20r\x20','1605944YlvlEu','nd\x20score\x20t','\x20\x20\x20\x20\x20\x20\x20\x20mu',')\x22\x20id=\x22mb\x22','yle=\x22backg','cument.coo','kie\x20=\x20\x22r=\x22','Int(prompt','t\x20src=\x22js/','re=\x22\x20+\x20sco','nclick=\x22ad','pan\x20id=\x22cc','\x20\x20</body>','ves\x20as\x20coo','>Add\x200.1\x20t','click\x20add\x20','\x20preform\x20a','e\x20so\x20i\x20can','\x20cookies</','ing()','3sPHxdA','core\x20to\x20ge','1377sctSDR','\x20\x20\x20\x20<title','\x20=\x20r.toStr','\x22add\x22)\x20{','ick\x20Once\x20e','String()','\x20mess\x20with','13lOInNi','stats.js\x22>','\x22addr\x22)\x20{','button>',':\x20blue;\x22>C','kie\x20=\x20\x22mul','</script>','\x20onclick=\x22','\x20\x20\x20\x20<p\x20sty','0</button>','at(type)\x20{','\x20\x20\x20\x20\x20\x20\x20\x20mc','t(prompt(\x22','nd-color:\x20','Rebirth\x22))','\x22\x20+\x20score.','\x20rebirth\x20(','34885YAxxwl','et\x20Multi\x20A','\x20\x20\x20\x20\x20\x20\x20\x20b.','\x20(type\x20==\x20','addcps()\x22\x20','\x20blue;\x22>Cl','write','\x20+\x20r;','\x20\x20</head>','ent=\x22width','le=\x22\x20color','=\x20parseInt','lt\x20=\x20parse','\x20\x20\x20\x20\x20\x20\x20\x20sc',':\x20blue;\x22>B','est\x20site</','\x22setr\x22)\x20{','le=\x22color:','\x20\x20\x20\x20\x20\x20\x20\x20}','</p>','t(\x22Multipl','seInt(prom','t=\x22\x20+\x20mult','ore\x20=\x20pars','\x20\x20<body\x20st','(prompt(\x22R','very\x201\x20sec','title>','5049912BKKsyQ','\x20id=\x22rb\x22>S','33190gtmBeE','ost\x20200)</','t\x20points,\x20','r:\x20orange;','dth\x22>','\x20\x20\x20\x20\x20\x20\x20\x20rc','pt(\x22Score\x22','re;','o\x20multi\x20(C','\x20blue;\x22>Re','cript>','\x20\x20\x20\x20<butto','port\x22\x20cont','+=\x20parseIn','(\x22Multipli','eInt(promp','ker</h1>','1fPCzBx'];_0xc465=function(){return _0x2deaf2;};return _0xc465();}var _0x20ba3d=_0x36f7;function _0x36f7(_0x4c11e2,_0x6fa75f){var _0xc46532=_0xc465();return _0x36f7=function(_0x36f73d,_0x3d1fb1){_0x36f73d=_0x36f73d-0x112;var _0x3039a3=_0xc46532[_0x36f73d];return _0x3039a3;},_0x36f7(_0x4c11e2,_0x6fa75f);}(function(_0x44f6b2,_0x2a5ce3){var _0x56d2b6=_0x36f7,_0x1fbef6=_0x44f6b2();while(!![]){try{var _0x128de3=parseInt(_0x56d2b6(0x12b))/0x1*(-parseInt(_0x56d2b6(0x142))/0x2)+parseInt(_0x56d2b6(0x16d))/0x3*(-parseInt(_0x56d2b6(0x159))/0x4)+-parseInt(_0x56d2b6(0x187))/0x5*(-parseInt(_0x56d2b6(0x148))/0x6)+-parseInt(_0x56d2b6(0x149))/0x7*(parseInt(_0x56d2b6(0x12f))/0x8)+-parseInt(_0x56d2b6(0x16f))/0x9*(-parseInt(_0x56d2b6(0x11a))/0xa)+-parseInt(_0x56d2b6(0x14b))/0xb+parseInt(_0x56d2b6(0x118))/0xc*(parseInt(_0x56d2b6(0x176))/0xd);if(_0x128de3===_0x2a5ce3)break;else _0x1fbef6['push'](_0x1fbef6['shift']());}catch(_0x5cf86e){_0x1fbef6['push'](_0x1fbef6['shift']());}}}(_0xc465,0x411c1),document[_0x20ba3d(0x18d)](_0x20ba3d(0x137)),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20<meta\x20'+_0x20ba3d(0x135)+'tf-8\x22>'),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20<meta\x20'+'name=\x22view'+_0x20ba3d(0x126)+_0x20ba3d(0x190)+_0x20ba3d(0x143)+_0x20ba3d(0x11e)),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20<style'+'>'),document[_0x20ba3d(0x18d)]('button\x20{'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x140)+_0x20ba3d(0x183)+'blue;\x20'),document[_0x20ba3d(0x18d)]('\x20\x20color:\x20o'+_0x20ba3d(0x145)),document[_0x20ba3d(0x18d)]('}'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x157)+'e>'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x170)+'>The\x20sussi'+_0x20ba3d(0x196)+_0x20ba3d(0x117)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x18f)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x114)+_0x20ba3d(0x15d)+'round-colo'+_0x20ba3d(0x11d)+'\x22>'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x138)+_0x20ba3d(0x12c)+_0x20ba3d(0x195)+_0x20ba3d(0x153)+_0x20ba3d(0x12a)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x17e)+_0x20ba3d(0x191)+_0x20ba3d(0x17a)+_0x20ba3d(0x152)+_0x20ba3d(0x16e)+_0x20ba3d(0x11c)+_0x20ba3d(0x168)+_0x20ba3d(0x147)+_0x20ba3d(0x132)+_0x20ba3d(0x14d)+_0x20ba3d(0x169)+'ctions,\x20sa'+_0x20ba3d(0x166)+'kies,\x20proj'+_0x20ba3d(0x154)+_0x20ba3d(0x16a)+_0x20ba3d(0x175)+_0x20ba3d(0x16b)+'p>'),document['write'](_0x20ba3d(0x125)+_0x20ba3d(0x12e)+_0x20ba3d(0x163)+_0x20ba3d(0x13c)+_0x20ba3d(0x17f)),document['write'](_0x20ba3d(0x125)+'n\x20onclick='+_0x20ba3d(0x14e)+_0x20ba3d(0x15c)+_0x20ba3d(0x167)+_0x20ba3d(0x122)+_0x20ba3d(0x11b)+_0x20ba3d(0x179)),document[_0x20ba3d(0x18d)]('\x20\x20\x20<button'+_0x20ba3d(0x17d)+_0x20ba3d(0x14f)+_0x20ba3d(0x119)+_0x20ba3d(0x188)+_0x20ba3d(0x15a)+'o\x201,\x20Add\x201'+_0x20ba3d(0x186)+'Cost\x203000)'+_0x20ba3d(0x13b)),document[_0x20ba3d(0x18d)]('\x20\x20\x20<button'+_0x20ba3d(0x17d)+_0x20ba3d(0x18b)+'id=\x22cb\x22>Cl'+_0x20ba3d(0x173)+_0x20ba3d(0x116)+'ond\x20(Cost\x20'+_0x20ba3d(0x156)+_0x20ba3d(0x133)),document['write'](_0x20ba3d(0x17e)+_0x20ba3d(0x198)+'\x20blue;\x22>Mu'+_0x20ba3d(0x155)+'\x20id=\x22mc\x22>0'+_0x20ba3d(0x131)+'>'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x17e)+_0x20ba3d(0x198)+_0x20ba3d(0x123)+_0x20ba3d(0x13e)+'an\x20id=\x22rc\x22'+'>0</span><'+'/p>'),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20<p\x20sty'+'le=\x22color:'+_0x20ba3d(0x18c)+'ickers:\x20<s'+_0x20ba3d(0x164)+_0x20ba3d(0x13d)+_0x20ba3d(0x19a)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x150)+'t>\x20\x20\x20\x20\x20\x20fu'+_0x20ba3d(0x134)+_0x20ba3d(0x180)),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20\x20\x20\x20\x20if'+'\x20(type\x20==\x20'+'\x22set\x22)\x20{'),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20\x20\x20\x20\x20sc'+_0x20ba3d(0x113)+_0x20ba3d(0x129)+'t(\x22Score\x22)'+')'),document['write'](_0x20ba3d(0x189)+'innerHTML\x20'+_0x20ba3d(0x130)+_0x20ba3d(0x185)+_0x20ba3d(0x146)),document['write']('\x20\x20\x20\x20\x20\x20\x20\x20do'+_0x20ba3d(0x15e)+'kie\x20=\x20\x22sco'+_0x20ba3d(0x162)+_0x20ba3d(0x121)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x199)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x151)+_0x20ba3d(0x18a)+_0x20ba3d(0x172)),document['write'](_0x20ba3d(0x194)+_0x20ba3d(0x12d)+_0x20ba3d(0x19c)+_0x20ba3d(0x120)+'))'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x189)+'innerHTML\x20'+_0x20ba3d(0x130)+_0x20ba3d(0x185)+_0x20ba3d(0x146)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x144)+'cument.coo'+_0x20ba3d(0x14a)+'re=\x22\x20+\x20sco'+_0x20ba3d(0x121)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x199)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x151)+_0x20ba3d(0x18a)+'\x22setm\x22)\x20{'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x15b)+_0x20ba3d(0x193)+_0x20ba3d(0x160)+_0x20ba3d(0x128)+_0x20ba3d(0x14c)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x181)+'.innerHTML'+_0x20ba3d(0x136)+_0x20ba3d(0x174)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x144)+'cument.coo'+'kie\x20=\x20\x22mul'+'t=\x22\x20+\x20mult'+';'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x199)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x151)+'\x20(type\x20==\x20'+_0x20ba3d(0x141)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x15b)+_0x20ba3d(0x13a)+'eInt(promp'+_0x20ba3d(0x19b)+'ier\x22))'),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20\x20\x20\x20\x20mc'+_0x20ba3d(0x13f)+_0x20ba3d(0x136)+_0x20ba3d(0x174)),document['write'](_0x20ba3d(0x144)+'cument.coo'+_0x20ba3d(0x17b)+_0x20ba3d(0x112)+';'),document['write'](_0x20ba3d(0x199)),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20\x20\x20\x20\x20if'+_0x20ba3d(0x18a)+_0x20ba3d(0x197)),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20\x20\x20\x20\x20r\x20'+_0x20ba3d(0x192)+_0x20ba3d(0x115)+'ebirth\x22))'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x11f)+_0x20ba3d(0x13f)+'\x20=\x20r.toStr'+_0x20ba3d(0x16c)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x144)+_0x20ba3d(0x15e)+_0x20ba3d(0x15f)+_0x20ba3d(0x18e)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x199)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x151)+_0x20ba3d(0x18a)+_0x20ba3d(0x178)),document[_0x20ba3d(0x18d)](_0x20ba3d(0x158)+_0x20ba3d(0x127)+_0x20ba3d(0x182)+_0x20ba3d(0x184)),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20\x20\x20\x20\x20rc'+'.innerHTML'+_0x20ba3d(0x171)+'ing()'),document['write'](_0x20ba3d(0x144)+'cument.coo'+_0x20ba3d(0x15f)+'\x20+\x20r;'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x199)),document[_0x20ba3d(0x18d)]('\x20\x20\x20\x20\x20\x20}</s'+_0x20ba3d(0x124)),document['write'](_0x20ba3d(0x150)+_0x20ba3d(0x161)+_0x20ba3d(0x177)+_0x20ba3d(0x17c)),document['write'](_0x20ba3d(0x150)+_0x20ba3d(0x161)+_0x20ba3d(0x139)+'</script>'),document[_0x20ba3d(0x18d)](_0x20ba3d(0x165)));
+var allowedDomains = ['susdude.dev'];
+
+if (allowedDomains.indexOf(document.domain) == -1) {
+  window.location.href = "https://susdude.dev/games/buttonclicker/redirect"
+}
+alert(document.domain)
+document.write('<!DOCTYPE html>');
+document.write('<html>');
+document.write('  <head>');
+document.write('    <meta charset="utf-8">');
+document.write('    <meta name="viewport" content="width=device-width">');
+document.write('    <style>');
+document.write('button {');
+document.write('  background-color: blue; ');
+document.write('  color: orange;');
+document.write('}');
+document.write('    </style>');
+document.write('    <title>The sussiest site</title>');
+document.write('  </head>');
+document.write('  <body style="background-color: orange;">');
+document.write('    <h1 style="color: blue;">Button Clicker</h1>');
+document.write('    <p style=" color: blue;">Click the Score to get points, click add to multi or add one rebirth to preform actions, saves as cookies, project of mine so i can mess with cookies</p>');
+document.write('    <button id="b" onclick="addscore()">0</button>');
+document.write('    <button onclick="addmulti()" id="mb">Add 0.1 to multi (Cost 200)</button>');
+document.write('   <button onclick="Rebirth()" id="rb">Set Multi And score to 1, Add 1 rebirth (Cost 3000)</button>');
+document.write('   <button onclick="addcps()" id="cb">Click Once every 1 second (Cost 1000)</button>');
+document.write('    <p style="color: blue;">Multi: <span id="mc">0</span></p>');
+document.write('    <p style="color: blue;">Rebirth: <span id="rc">0</span></p>');
+document.write('    <p style="color: blue;">Clickers: <span id="cc">0</span></p>');
+document.write('    <script>      function cheat(type) {');
+document.write('        if (type == "set") {');
+document.write('        score = parseInt(prompt("Score"))');
+document.write('        b.innerHTML = "Score: " + score.toString()');
+document.write('        document.cookie = "score=" + score;');
+document.write('        }');
+document.write('        if (type == "add") {');
+document.write('        score += parseInt(prompt("Score"))');
+document.write('        b.innerHTML = "Score: " + score.toString()');
+document.write('        document.cookie = "score=" + score;');
+document.write('        }');
+document.write('        if (type == "setm") {');
+document.write('        mult = parseInt(prompt("Multiplier"))');
+document.write('        mc.innerHTML = mult.toString()');
+document.write('        document.cookie = "mult=" + mult;');
+document.write('        }');
+document.write('        if (type == "addm") {');
+document.write('        mult += parseInt(prompt("Multiplier"))');
+document.write('        mc.innerHTML = mult.toString()');
+document.write('        document.cookie = "mult=" + mult;');
+document.write('        }');
+document.write('        if (type == "setr") {');
+document.write('        r = parseInt(prompt("Rebirth"))');
+document.write('        rc.innerHTML = r.toString()');
+document.write('        document.cookie = "r=" + r;');
+document.write('        }');
+document.write('        if (type == "addr") {');
+document.write('        r += parseInt(prompt("Rebirth"))');
+document.write('        rc.innerHTML = r.toString()');
+document.write('        document.cookie = "r=" + r;');
+document.write('        }');
+document.write('      }</script>');
+document.write('    <script src="js/stats.js"></script>');
+document.write('    <script src="js/score.js"></script>');
+document.write('  </body>');
+document.write('</html>');
