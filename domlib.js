10576868228aae432e7e564d456686dd13837094
Sus Dude <103893247+sussybakadude@users.noreply.github.com>
2022-04-18 19:08:35 +0000

    add EOS, domlib and modify files

---
+++ domlib.js
@@ -0,0 +1,24 @@
+function getinput(id) {
+  elm = document.getElementById(id)
+  return elm.value
+}
+function getHTML(id) {
+  elm = document.getElementById(id)
+  return elm.innerhtml
+}
+function get(id) {
+  elm = document.getElementById(id)
+  return elm
+}
+function save(id, value) {
+  elm = document.getElementById(id)
+  elm.innerHTML = value
+}
+function saveinput(id, value) {
+  elm = document.getElementById(id)
+  elm.value = value
+}
+function savehref(id, value) {
+  elm = document.getElementById(id)
+  elm.href = value
+}
