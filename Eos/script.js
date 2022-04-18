0a686be3efda4d04d68914fe48316634785ba830
Sus Dude <103893247+sussybakadude@users.noreply.github.com>
2022-04-18 19:11:58 +0000

    test AOS

--- Eos/script.js
+++ Eos/script.js
@@ -1,5 +1,7 @@
+function submit() {
 a = getinput("in")
 b = a.replace(" ", "+")
 c = encodeURIComponent(b)
 d = c.replace("?x=", "")
 savehref("out", "https://www.google.com/search?q=site%3A.org+" + sout +  "&lr=lang_en&tbs=sur%3Afmc%2Cqdr%3Ay%2Clr%3Alang_1en&ei=vLVdYqehB_TBytMPvMysiAQ&ved=0ahUKEwin77aypp73AhX0oHIEHTwmC0EQ4dUDCA4&uact=5&oq=site%3A.org+site&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGAFKBAhGGABQzEFY-VZgyFpoAHAAeACAAdADiAHqCZIBBzYuMy0xLjGYAQCgAQHAAQE&sclient=gws-wiz&safe=active&ssui=on")
+}
